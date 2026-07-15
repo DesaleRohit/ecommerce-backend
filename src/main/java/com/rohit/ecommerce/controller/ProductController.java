9eec1e29a93e1b08b8042e3a92bfe5185c96bef5
DesaleRohit <rohitdesale2005@gmail.com>
2026-07-15 12:54:59 +0530

    [MODIFIED]: Rohit - add GetMapping to retrive data

--- src/main/java/com/rohit/ecommerce/controller/ProductController.java
+++ src/main/java/com/rohit/ecommerce/controller/ProductController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -25,4 +24,8 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
 }
--- src/main/java/com/rohit/ecommerce/service/ProductService.java
+++ src/main/java/com/rohit/ecommerce/service/ProductService.java
@@ -7,6 +7,8 @@ import com.rohit.ecommerce.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -34,5 +36,11 @@ public class ProductService {
                 .stockQuantity(product.getStockQuantity())
                 .category(product.getCategory())
                 .build();
+    }
+
+    public List<ProductResponseDTO> getAllProducts() {
+        return productRepo.findAll().stream()
+                .map(this::toResponseDTO)
+                .toList();
     }
 }
